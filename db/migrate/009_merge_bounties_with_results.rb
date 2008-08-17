59b3cffe8f049cfbd8b37e6a4b0c4330c7821e2a
kopler <kopler@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-17 19:17:22 +0000

    - gave payout_structures a series_id, gave tournaments a payout_structure_id - made results creation more intuitive (see migration 10)
    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@27 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- db/migrate/009_merge_bounties_with_results.rb
+++ db/migrate/009_merge_bounties_with_results.rb
@@ -1,8 +1,8 @@
 class MergeBountiesWithResults < ActiveRecord::Migration
   def self.up
-    #drop_table :bounties
-    #add_column :results, :bounty_collector_id, :integer
-    #add_column :results, :tournament_id, :integer
+    drop_table :bounties
+    add_column :results, :bounty_collector_id, :integer
+    add_column :results, :tournament_id, :integer
     add_column :tournaments, :payout_structure_id, :integer
     add_column :payout_structures, :series_id, :integer
   end
