35a6f45000763a6bc84df46d2abcb3cdfeedb71b
kopler <kopler@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-17 19:13:54 +0000

    - gave payout_structures a series_id, gave tournaments a payout_structure_id - made results creation more intuitive (see migration 10)
    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@26 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- db/migrate/010_create_test_data.rb
+++ db/migrate/010_create_test_data.rb
@@ -3,9 +3,6 @@ class CreateTestData < ActiveRecord::Migration
   # To test the automation of player stats generation (for player_stats table),
   # this can be run, reversed, and re-run as needed
   def self.up
-    Series.destroy_all
-    Player.destroy_all
-    
     p1 = Player.create!(:first_name=>"Eric", :last_name=>"Tipton")
     p2 = Player.create!(:first_name=>"Ryan", :last_name=>"Johns")
     p3 = Player.create!(:first_name=>"Amir", :last_name=>"Manji")
@@ -17,7 +14,7 @@ class CreateTestData < ActiveRecord::Migration
     pay2 = Payout.create!(:payout_structure=>ps, :place=>2, :points=>50, :money=>200)
     pay3 = Payout.create!(:payout_structure=>ps, :place=>3, :points=>25, :money=>100)
     
-    t = Tournament.create!(:series_id=>s.id, :tournament_date=>Time.now, :num_entrants=>3)
+    t = Tournament.create!(:series=>s, :tournament_date=>Time.now, :num_entrants=>3)
     
     result3 = Result.create!(:tournament=>t, :player=>p3, :place=>3, :bounty_collector=>p1, :fee_paid=>true)
     result2 = Result.create!(:tournament=>t, :player=>p2, :place=>2, :bounty_collector=>p1, :fee_paid=>true)
