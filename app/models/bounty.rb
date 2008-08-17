8a7d7e8a4c1f13680e153773263ff1c53a75abfa
kopler <kopler@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-17 00:45:12 +0000

    no need to comment
    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@15 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -1,6 +1,27 @@
 class Bounty < ActiveRecord::Base
   
-  belongs_to :player, :foreign_key => "pwner_id"
-  
   validates_presence_of :bitch_id, :pwner_id
+  
+  def pwner
+    Player.find_by_id(self.pwner_id)
+  end
+  
+  def bitch
+    Player.find_by_id(self.bitch_id)
+  end
+  
+  def pwner=(player)
+    if !player.is_a? Player
+      return false
+    end
+    self.pwner_id = player.id
+  end
+  
+  def bitch=(player)
+    if !player.is_a? Player
+      return false
+    end
+    self.bitch_id = player.id
+  end
+  
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,12 @@
 class Player < ActiveRecord::Base
 
   has_many :results
-  has_many :bounties
   has_one :player_stats
   
   validates_presence_of :first_name, :last_name
+  
+  def bounties
+    Bounty.find_all_by_pwner_id(self.id)
+  end
+  
 end
