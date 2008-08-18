8a10d1593a12b0795a74ba1a55e8cf4919ddd60c
kopler <kopler@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-18 07:58:04 +0000

    amir sucks
    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@34 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- app/helpers/standings_helper.rb
+++ app/helpers/standings_helper.rb
@@ -1,8 +1,8 @@
 module StandingsHelper
   
-  def sort_column(col_title, db_field_name=nil)
+  def sort_column(col_title, db_field_name=nil, action="index")
     db_field_name ||= col_title.gsub("Number", "num").gsub("Average", "avg").gsub("of ", "").gsub(" ", "_").downcase
-    link_to col_title, {:action=>'index', :order_by=>db_field_name, :order=>get_reverse_order(db_field_name)}
+    link_to col_title, {:action=>action, :order_by=>db_field_name, :order=>get_reverse_order(db_field_name)}
   end
   
 end
