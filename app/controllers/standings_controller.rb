730f756692cb49f8f42814c4b5c5ee7610ad34ca
kopler <kopler@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-18 11:12:42 +0000

    Added sort arrows... they still aren't exactly layed out right though
    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@37 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -1,22 +1,26 @@
 class StandingsController < ApplicationController
 
   def index
-    order_by = params[:order_by] || "rank"
-    order = params[:order] || "asc"
-
-    if order_by == "name" and ["asc", "desc"].include? order
-      order_by = "last_name"
-    elsif !PlayerStat.new.respond_to? order_by or !["asc", "desc"].include? order
-      order_by = "rank"
-      order = "asc"
+    @order_by = params[:order_by] || "rank"
+    @order = params[:order] || "asc"
+    
+    if @order_by == "name" and ["asc", "desc"].include? @order
+      @order_by = "last_name"
+    elsif !PlayerStat.new.respond_to? @order_by or !["asc", "desc"].include? @order
+      @order_by = "rank"
+      @order = "asc"
     end
     
-    order_sql = "#{order_by} #{order}"
-    order_sql << ", first_name #{order}" if order_by == "last_name"
-    order_sql << ", rank asc" if order_by != "rank"
+    order_sql = "#{@order_by} #{@order}"
+    order_sql << ", first_name #{@order}" if @order_by == "last_name"
+    order_sql << ", rank asc" if @order_by != "rank"
     
-    series = Series.find(:first, :conditions => 'is_complete = false')
-    @player_stats = PlayerStat.find(:all, :include=>:player, :conditions => "series_id = #{series.id} and rank > 0", :order => order_sql) rescue []
+    if @order_by == "last_name"
+      @order_by = "name"
+    end
+    
+    @series = Series.find(:first, :conditions => 'is_complete = false')
+    @player_stats = PlayerStat.find(:all, :include=>:player, :conditions => "series_id = #{@series.id} and rank > 0", :order => order_sql) rescue []
   end
 
 end
--- app/helpers/standings_helper.rb
+++ app/helpers/standings_helper.rb
@@ -2,7 +2,9 @@ module StandingsHelper
   
   def sort_column(col_title, db_field_name=nil, action="index")
     db_field_name ||= col_title.gsub("Number", "num").gsub("Average", "avg").gsub("of ", "").gsub(" ", "_").downcase
-    link_to col_title, {:action=>action, :order_by=>db_field_name, :order=>get_reverse_order(db_field_name)}
+    html = link_to col_title, {:action=>action, :order_by=>db_field_name, :order=>get_reverse_order(db_field_name)}
+    html << (@order_by == db_field_name ? (@order == 'asc' ? '<img src="/images/down_arrow.gif" />' : '<img src="/images/up_arrow.gif" />') : '')
+    html
   end
   
 end
