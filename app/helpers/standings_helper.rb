ecef645a72bbd88661a34cf79bbef171c5b6b44f
ryan <ryan@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-21 01:40:42 +0000

    Changed column headers and sorting method to require the db_field_name Also changed the sort arrows
    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@43 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- app/helpers/standings_helper.rb
+++ app/helpers/standings_helper.rb
@@ -1,7 +1,6 @@
 module StandingsHelper
   
-  def sort_column(col_title, db_field_name=nil, action="index")
-    db_field_name ||= col_title.gsub("Number", "num").gsub("Average", "avg").gsub("of ", "").gsub(" ", "_").downcase
+  def sort_column(col_title, db_field_name, action="index")
     html = link_to col_title, {:action=>action, :order_by=>db_field_name, :order=>get_reverse_order(db_field_name)}
     html << (@order_by == db_field_name ? (@order == 'asc' ? '<img src="/images/down_arrow.gif" />' : '<img src="/images/up_arrow.gif" />') : '')
     html
