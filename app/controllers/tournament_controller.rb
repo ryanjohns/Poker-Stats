20ae9f71362d93872e4159daf52cd505569eece4
kopler <kopler@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-22 08:10:22 +0000

    commitosaurus rex
    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@53 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- app/controllers/tournament_controller.rb
+++ app/controllers/tournament_controller.rb
@@ -1,2 +1,28 @@
 class TournamentController < ApplicationController
+  
+  def index
+    @series = Series.find(:all)
+    
+    @tournaments = []
+    @series.each do |series|
+      @tournaments[series.id] = Tournament.find_all_by_series_id(series.id)
+    end
+    
+  end
+  
+  def view
+    redirect_to :action => 'index' and return unless @tournament = Tournament.find(params[:id]) rescue false
+    @results = Result.find_all_by_tournament_id(params[:id], :order => 'payout_id ASC')
+    
+    @results.each do |result|
+      unless result.bounty_collector_id.blank?
+        bounty_collector = @results.find { |bc| bc.player_id == result.bounty_collector_id }
+        bounty_collector['bounties'] = bounty_collector['bounties'] + 1 rescue 1
+      end
+      result['bounties'] ||= 0
+    end
+    
+  end
+  
+  
 end
