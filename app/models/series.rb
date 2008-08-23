8140affd61b1002b3f695dbda6318ed9f53642a0
kopler <kopler@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-23 22:44:12 +0000

    - implemented a many-to-many relationship between payout_structures and series' (don't ask me how!)
    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@74 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- app/models/series.rb
+++ app/models/series.rb
@@ -2,12 +2,17 @@ class Series < ActiveRecord::Base
   
   has_many :tournaments, :dependent=>:destroy
   has_many :player_stats, :dependent=>:destroy
-  has_many :payout_structures #, :dependent=>:destroy
   
+  attr_reader(:payout_structures_attr)
   validates_presence_of :name
   
+  def payout_structures
+    return @payout_structures_attr if @payout_structures_attr
+    @payout_structures_attr = PayoutStructure.find_all_by_series_id(self.id)
+  end
+  
   def destroy
-    PayoutStructure.find_all_by_series_id(self.id).each do |ps|
+    payout_structures.each do |ps|
       ps.remove_series_id(self.id)
       ps.destroy if ps.series_ids.empty?
     end
