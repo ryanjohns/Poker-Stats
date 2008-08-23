488c8642a1c89cee7caed515a247fc1298bf7df6
kopler <kopler@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-23 22:39:48 +0000

    fixed the tournaments and players views
    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@73 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- app/models/result.rb
+++ app/models/result.rb
@@ -80,7 +80,7 @@ class Result < ActiveRecord::Base
       bounty_stats.update_stats
     end
     
-    #now update this player's stats
+    # now update this player's stats
     stats = PlayerStat.find_or_create_by_player_id_and_series_id(player_id, tournament.series_id)
     stats.update_stats
     
