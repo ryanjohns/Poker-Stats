3d3ca3fd17d8710dd7848829d1fbc075e297ff80
kopler <kopler@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-23 02:59:36 +0000

    remove bounty calculations from tournament controller
    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@67 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- app/controllers/tournament_controller.rb
+++ app/controllers/tournament_controller.rb
@@ -14,15 +14,6 @@ class TournamentController < ApplicationController
     redirect_to :action => 'index' and return unless @tournament = Tournament.find(params[:id]) rescue false
     @results = Result.find_all_by_tournament_id(params[:id], :order => 'payout_id ASC')
     @results.sort { |a,b| a.payout.place  <=> b.payout.place }
-    
-    @results.each do |result|
-      unless result.bounty_collector_id.blank?
-        bounty_collector = @results.find { |bc| bc.player_id == result.bounty_collector_id }
-        bounty_collector['bounties'] = bounty_collector['bounties'] + 1 rescue 1
-      end
-      result['bounties'] ||= 0
-    end
-    
   end
   
   
