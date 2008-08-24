754b48f6fa9721d1aa5965958413aba301f8faaf
kopler <kopler@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-24 10:52:54 +0000

    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@107 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -15,16 +15,21 @@ class PlayerController < ApplicationController
   def view
     redirect_to :controller =>'standings', :action => 'index' and return unless @player = Player.find(params[:id]) rescue false
     @results = []
+    
+    series_index = []
+    count = 0
+    Series.find(:all, :order => 'start_date DESC').each do |series|
+      series_index[series.id] = count
+      count += 1
+    end
+    
     Result.find_all_by_player_id(params[:id]).each do |result|
       begin
-        @results[result.tournament.series.id] << result
+        @results[series_index[result.tournament.series.id]] << result
       rescue
-        @results[result.tournament.series.id] = [result]
+        @results[series_index[result.tournament.series.id]] = [result]
       end
     end
     @results.compact!
-    @results.each do |series_results|
-      series_results.sort { |a,b| a.tournament.tournament_date <=> b.tournament.tournament_date }
-    end
   end
 end
