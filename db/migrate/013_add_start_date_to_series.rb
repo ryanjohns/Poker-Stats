5cc7d62fac2d218ff00fdad1c5ea1ff655b07de0
kopler <kopler@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-08-24 01:16:16 +0000

    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@81 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- db/migrate/013_add_start_date_to_series.rb
+++ db/migrate/013_add_start_date_to_series.rb
@@ -1,6 +1,6 @@
 class AddStartDateToSeries < ActiveRecord::Migration
   def self.up
-    add_column :series, :start_date, :datetime
+    add_column :series, :start_date, :datetime, :null => false
     series = Series.find_by_name("Season 3")
     series.start_date = DateTime.parse("07/30/2008")
     series.save!
