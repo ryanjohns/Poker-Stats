a62035043f4b7757fd0609b74a60acc0ea680b0d
ryan <ryan@fbffe032-b7cb-47bf-8c5c-4514d0c4e17a>
2008-10-09 06:14:09 +0000

    update results entry
    git-svn-id: svn+ssh://192.168.1.3/home/svn/poker_stats/trunk@137 fbffe032-b7cb-47bf-8c5c-4514d0c4e17a

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -6,14 +6,12 @@ class ResultController < ApplicationController
     flash.now[:error] = "You must give a valid tournament_id" and return if @tournament.nil?
     flash.now[:error] = "Tournament is complete" and return if @tournament.num_entrants == Result.find_all_by_tournament_id(@tournament.id).size
     @players = Player.find(:all, :order => 'last_name asc')
-    @current_results = Result.find_all_by_tournament_id(@tournament.id, :order => "id desc")
     if request.post?
       player = Player.find_by_id(params[:player_id].to_i)
       bounty_collector = Player.find_by_id(params[:bounty_collector_id].to_i)
       fee_paid = (params[:fee_paid] == "1" ? true : false)
       Result.create!(:tournament => @tournament, :player => player, :bounty_collector => bounty_collector, :fee_paid => fee_paid, :place => params[:place].to_i, :update_player_stats => true)
-      redirect_to :controller => 'tournament', :action => 'view', :id => @tournament.id and return if bounty_collector.nil?
-      @current_results = Result.find_all_by_tournament_id(@tournament.id, :order => "id desc")
+      redirect_to :controller => 'tournament', :action => 'view', :id => @tournament.id if bounty_collector.nil?
     end
   end
   
